5f13ccda7a78713696cfdfc4e762f189fdf2bd06
Ritik Chourasiya <ritvikchoure65@gmail.com>
2021-01-01 10:30:48 +0530

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,152 @@
+using namespace std;
+
+#include<iostream>
+
+int main()
+{
+
+// --- Array Insertion (Start) -----------------------------------------
+    
+    // int i;
+
+    // int n = 5;
+
+    // int arr[n] = {60, 70, 80, 90, 100};
+
+    // int k=3;
+
+    // int item = 85;
+
+    // int j=n;
+
+
+    // cout<<"Original Array"<<endl;
+    // for(i=0; i<n; i++)
+    // {
+    //     cout<<"array of ["<<i<<"] : "<<arr[i]<<endl;
+    // }
+
+    // n=n+1;
+
+    // while(j>=k)
+    // {
+    //     arr[j+1]=arr[j];
+    //     j = j-1;
+    // }
+
+    // arr[k] = item;
+
+    // cout<<"Array after Insertion :"<<endl;
+
+    // for(i=0; i<n; i++)
+    // {
+    //     cout<<"array of ["<<i<<"] : "<<arr[i]<<endl;
+    // }
+
+
+// --- Array Insertion (End) -----------------------------------------
+
+
+// --- Array Deletion (Start) -----------------------------------------
+
+
+   /* int n = 5;
+
+    int arr[n] = {60, 70, 80, 90, 100};
+
+    int k = 3;
+
+    int i, j;
+
+    cout<<"Original Array"<<endl;
+    for(i=0; i<n; i++)
+    {
+        cout<<"array of ["<<i<<"] : "<<arr[i]<<endl;
+    }
+
+    while(k<n)
+    {
+        arr[k-1] = arr[k];
+        k = k+1;
+    }    
+
+    n = n-1;
+
+    cout<<"Array after Insertion :"<<endl;
+
+    for(i=0; i<n; i++)
+    {
+        cout<<"array of ["<<i<<"] : "<<arr[i]<<endl;
+    } */
+
+// --- Array Deletion (End) -----------------------------------------
+
+// --- Array Searching (Start) -----------------------------------------
+
+    /*
+
+    int n = 5;
+
+    int arr[n] = {100, 200, 300, 400, 500};
+
+    int item = 300;
+
+    cout<<"Original Array :"<<endl;
+
+    for(int i = 0; i<n; i++)
+    {
+        cout<<"Array at "<<i<<" index is : "<<arr[i]<<endl;
+    }
+
+    int j=0;
+
+    while(j<n)
+    {
+        if(arr[j]==item)
+        {
+            break;
+        }
+
+        j++;
+    }
+
+    cout<<"Elemet "<<item<<" Found at "<<j+1<<" position"<<endl;
+
+    */
+
+// --- Array Searching (End) -----------------------------------------
+
+// --- Array Updation (Start) -----------------------------------------
+
+    /*
+    
+    int n = 5;
+
+    int arr[n] = {55, 65, 70, 85, 95};
+
+    int k = 3;
+
+    int item = 75;
+
+    cout<<"Original Array :"<<endl;
+
+    for(int i = 0; i<n; i++)
+    {
+        cout<<"Array at "<<i<<" index is : "<<arr[i]<<endl;
+    }
+
+    arr[k-1] = item;
+
+    cout<<"Array after Insertion :"<<endl;
+
+    for(int i=0; i<n; i++)
+    {
+        cout<<"array of ["<<i<<"] : "<<arr[i]<<endl;
+    }
+
+    */
+
+// --- Array Updation (End) -----------------------------------------
+
+
+}
